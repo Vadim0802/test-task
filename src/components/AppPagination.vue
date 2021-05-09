1d6358397cde001cb6abc6eeaefc9a100ff6dadd
Vadim Bodianskii <v.bodyansky@yandex.ru>
2021-05-10 09:22:34 +1000

    add pagination

---
+++ src/components/AppPagination.vue
@@ -0,0 +1,87 @@
+<template>
+  <nav class="d-flex align-items-end" aria-label="Page navigation example">
+    <ul class="pagination">
+      <li
+        class="page-item"
+        v-if="hasPreviousPage"
+        @click="changeCurrentPage(currentPage - 1)"
+      >
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li
+        class="page-item"
+        v-if="hasPreviousPage"
+        @click="changeCurrentPage(currentPage - 1)"
+      >
+        <a class="page-link" href="#">{{ currentPage - 1 }}</a>
+      </li>
+      <li class="page-item active">
+        <a class="page-link" href="#">{{ currentPage }}</a>
+      </li>
+      <li
+        class="page-item"
+        v-if="hasNextPage"
+        @click="changeCurrentPage(currentPage + 1)"
+      >
+        <a class="page-link" href="#">{{ currentPage + 1 }}</a>
+      </li>
+      <li
+        class="page-item"
+        v-if="hasNextPage"
+        @click="changeCurrentPage(currentPage + 1)"
+      >
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    tableRowsCounter: {
+      type: Number,
+      required: true,
+    },
+    perPage: {
+      type: Number,
+      required: true,
+    },
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  emits: {
+    "change-page": null,
+  },
+
+  methods: {
+    changeCurrentPage(numPage) {
+      this.$emit("change-page", numPage);
+    },
+  },
+
+  computed: {
+    maxPages() {
+      return (
+        this.tableRowsCounter / this.perPage +
+        (this.tableRowsCounter % this.perPage === 0)
+      );
+    },
+
+    hasNextPage() {
+      return this.currentPage < this.maxPages;
+    },
+
+    hasPreviousPage() {
+      return this.currentPage > 1;
+    },
+  },
+};
+</script>
--- src/components/TheTable.vue
+++ src/components/TheTable.vue
@@ -1,4 +1,23 @@
 <template>
+  <div class="d-flex justify-content-between">
+    <div>
+      <label class="h4" for="per-page">Per page</label>
+      <select
+        id="per-page"
+        class="form-select mb-3"
+        aria-label="Default select example"
+        v-model.number="perPage"
+      >
+        <option v-for="n in 9" :key="n" :value="n">{{ n }}</option>
+      </select>
+    </div>
+    <app-pagination
+      :tableRowsCounter="users.length"
+      :perPage="perPage"
+      :currentPage="currentPage"
+      @change-page="changePage"
+    ></app-pagination>
+  </div>
   <table class="table table-striped">
     <thead class="table-dark">
       <tr>
@@ -10,8 +29,8 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(item, idx) in users" :key="idx">
-        <th scope="row">{{ idx }}</th>
+      <tr v-for="(item, idx) in usersOnPage" :key="idx">
+        <th scope="row">{{ currentBeginIndexesOfPage + idx + 1 }}</th>
         <td>{{ item.name }}</td>
         <td>{{ item.height }}</td>
         <td>{{ item.mass }}</td>
@@ -22,19 +41,45 @@
 </template>
 
 <script>
+import AppPagination from "./AppPagination.vue";
 import { getUsers } from "../api.js";
 
 export default {
+  components: {
+    AppPagination,
+  },
+
   data() {
     return {
       users: [],
+      perPage: 9,
+      currentPage: 1,
     };
   },
 
-  mounted() {
-    getUsers("https://swapi.dev/api/people").then((res) => {
+  async created() {
+    await getUsers("https://swapi.dev/api/people").then((res) => {
       this.users = res;
     });
+  },
+
+  methods: {
+    changePage(newPage) {
+      this.currentPage = newPage;
+    },
+  },
+
+  computed: {
+    usersOnPage() {
+      const begin = this.perPage * this.currentPage - this.perPage;
+      const end = this.perPage * this.currentPage;
+
+      return this.users.slice(begin, end);
+    },
+
+    currentBeginIndexesOfPage() {
+      return this.perPage * this.currentPage - this.perPage;
+    },
   },
 };
 </script>
